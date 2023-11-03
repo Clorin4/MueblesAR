d6fa3c7b4c17bfecc78172a79ec1e149b90b76ae
Isaac <isaacrodacu11@gmail.com>
2023-11-03 15:47:05 -0600

    3 casi casi

--- Assets/Scripts/FloorPlacement.cs
+++ Assets/Scripts/FloorPlacement.cs
@@ -1,37 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
+using UnityEngine.SceneManagement;
 
 public class FloorPlacement : MonoBehaviour
 {
-
-    public ARRaycastManager raycastManager;
-    public GameObject objectToPlace;
-
-
-    private void Update()
+    public void Entrar()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            //Raycast al piso mami
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            List<ARRaycastHit> hits = new List<ARRaycastHit>();
-
-            if (raycastManager.Raycast(ray, hits, TrackableType.PlaneWithinPolygon))
-            {
-                //Obtener planou detectao
-                Pose hitPose = hits[0].pose;
-                Debug.Log("Plano detectao" + hitPose.position);
-
-                //Colocar el mueble
-                Instantiate(objectToPlace, hitPose.position, hitPose.rotation);
-                Debug.Log("Obj instantiadou en " + hitPose.position);
-            }
-
-            Debug.Log("Nada pa");
-        }
+        SceneManager.LoadScene(1);
     }
-
+    
 }
