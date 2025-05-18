2ebe4fea41af0108e1c235be8bd6c0b4debd8536
Isaac <isaacrodacu11@gmail.com>
2025-05-17 20:41:45 -0600

    WIWIWIWI

--- Assets/Scripts/ImageTargetDetection.cs
+++ Assets/Scripts/ImageTargetDetection.cs
@@ -1,48 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-public class ImageTargetDetection : MonoBehaviour
+public class SimpleImageTarget : MonoBehaviour
 {
-    [SerializeField] ARTrackedImageManager trackedImageManager;
-    [SerializeField] GameObject objectToPlace;
+    [SerializeField] private ARTrackedImageManager trackedImageManager;
 
-    private GameObject targetImageObject;
+    [System.Serializable]
+    public class ImagePrefabPair
+    {
+        public string imageName;          // Nombre exacto en la XRReferenceImageLibrary
+        public GameObject prefabToPlace;  // Prefab que se mostrará cuando se detecte la imagen
+    }
+
+    [SerializeField] private List<ImagePrefabPair> imagePrefabPairs;
+
+    private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
 
     private void OnEnable()
     {
-        trackedImageManager.trackedImagesChanged += OnTrackedImageChanged;
+        trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
     }
-
 
     private void OnDisable()
     {
-        trackedImageManager.trackedImagesChanged -= OnTrackedImageChanged;
+        trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
     }
 
-
-    void OnTrackedImageChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
-        foreach(var trackedImage in eventArgs.added)
+        foreach (var trackedImage in eventArgs.added)
         {
-            if(trackedImage.referenceImage.name == "Cloro")
-            {
-                targetImageObject = trackedImage.gameObject;
-            }
-            
+            HandleImage(trackedImage);
         }
+
         foreach (var trackedImage in eventArgs.updated)
         {
-            //Acciones addicionaless en img act
-            
+            HandleImage(trackedImage);
         }
+
         foreach (var trackedImage in eventArgs.removed)
         {
-            //acciones cuando imagen eliminada
+            string imageName = trackedImage.referenceImage.name;
+            if (spawnedPrefabs.ContainsKey(imageName))
+            {
+                Destroy(spawnedPrefabs[imageName]);
+                spawnedPrefabs.Remove(imageName);
+            }
         }
     }
 
+    void HandleImage(ARTrackedImage trackedImage)
+    {
+        string imageName = trackedImage.referenceImage.name;
 
+        foreach (var pair in imagePrefabPairs)
+        {
+            if (pair.imageName == imageName)
+            {
+                if (!spawnedPrefabs.ContainsKey(imageName))
+                {
+                    GameObject spawned = Instantiate(pair.prefabToPlace, trackedImage.transform);
+                    spawned.transform.localPosition = Vector3.zero;
+                    spawned.transform.localRotation = Quaternion.identity;
+                    spawned.transform.localScale = Vector3.one * 0.5f;  // Escala 0.5
+                    spawnedPrefabs[imageName] = spawned;
+                }
+                else
+                {
+                    spawnedPrefabs[imageName].SetActive(true);
+                    spawnedPrefabs[imageName].transform.position = trackedImage.transform.position;
+                }
+                break;
+            }
+        }
+    }
 }
