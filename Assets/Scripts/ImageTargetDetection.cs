b0ef7b64711343c8de0ed28d6f71cfd75aff929f
Isaac <isaacrodacu11@gmail.com>
2025-05-18 22:22:03 -0600

    SemiFinal

--- Assets/Scripts/ImageTargetDetection.cs
+++ Assets/Scripts/ImageTargetDetection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
@@ -6,13 +7,21 @@ using UnityEngine.XR.ARSubsystems;
 public class SimpleImageTarget : MonoBehaviour
 {
     [SerializeField] private ARTrackedImageManager trackedImageManager;
+    [SerializeField] private TMPro.TextMeshProUGUI infoTextUI;
+    [SerializeField] private GameObject PanelText;
 
     [System.Serializable]
     public class ImagePrefabPair
     {
-        public string imageName;          // Nombre exacto en la XRReferenceImageLibrary
-        public GameObject prefabToPlace;  // Prefab que se mostrará cuando se detecte la imagen
+        public string imageName;
+        public GameObject prefabToPlace;
+        public Vector3 finalScale = Vector3.one * 0.5f;
+        public Vector3 finalRotationEuler = Vector3.zero;
+
+        [TextArea]
+        public string infoText; // Texto informativo a mostrar
     }
+
 
     [SerializeField] private List<ImagePrefabPair> imagePrefabPairs;
 
@@ -59,21 +68,61 @@ public class SimpleImageTarget : MonoBehaviour
         {
             if (pair.imageName == imageName)
             {
-                if (!spawnedPrefabs.ContainsKey(imageName))
+                if (trackedImage.trackingState == TrackingState.Tracking)
                 {
-                    GameObject spawned = Instantiate(pair.prefabToPlace, trackedImage.transform);
-                    spawned.transform.localPosition = Vector3.zero;
-                    spawned.transform.localRotation = Quaternion.identity;
-                    spawned.transform.localScale = Vector3.one * 0.01f;  // Escala 0.5
-                    spawnedPrefabs[imageName] = spawned;
+                    if (!spawnedPrefabs.ContainsKey(imageName))
+                    {
+                        GameObject spawned = Instantiate(pair.prefabToPlace, trackedImage.transform);
+                        spawned.transform.localPosition = new Vector3(0f, 0.05f, 0f);
+                        spawned.transform.localRotation = Quaternion.Euler(pair.finalRotationEuler);
+                        spawned.transform.localScale = Vector3.zero;
+
+                        spawnedPrefabs[imageName] = spawned;
+                        StartCoroutine(ScaleUp(spawned, pair.finalScale, 0.25f));
+
+                        // Mostrar texto
+                        if (infoTextUI != null)
+                        {
+                            PanelText.SetActive(true);
+                            infoTextUI.text = pair.infoText;
+                            infoTextUI.gameObject.SetActive(true);
+                        }
+                    }
                 }
                 else
                 {
-                    spawnedPrefabs[imageName].SetActive(true);
-                    spawnedPrefabs[imageName].transform.position = trackedImage.transform.position;
+                    if (spawnedPrefabs.ContainsKey(imageName))
+                    {
+                        Destroy(spawnedPrefabs[imageName]);
+                        spawnedPrefabs.Remove(imageName);
+
+                        // Ocultar texto
+                        if (infoTextUI != null)
+                        {
+                            PanelText.SetActive(false);
+                            infoTextUI.gameObject.SetActive(false);
+                        }
+                    }
                 }
+
                 break;
             }
         }
+    }
+
+
+    IEnumerator ScaleUp(GameObject obj, Vector3 targetScale, float duration)
+    {
+        Vector3 initialScale = obj.transform.localScale;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            obj.transform.localScale = Vector3.Lerp(initialScale, targetScale, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        obj.transform.localScale = targetScale;
     }
 }
